57ca0a6376194d6c2bc424e15bed497f30c44afd
YQ-zhishen <yangqian_1015@icloud.com>
2021-10-29 15:50:26 +0800

    Optimized dependency for account

--- backend/academic/modules/account/src/main/java/com/buaa/academic/account/controller/AccountController.java
+++ backend/academic/modules/account/src/main/java/com/buaa/academic/account/controller/AccountController.java
@@ -6,11 +6,12 @@ import com.buaa.academic.document.entity.User;
 import com.buaa.academic.model.security.Authority;
 import com.buaa.academic.model.web.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
