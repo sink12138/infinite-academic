35144bc384ae985fb464cf4ce65bbff1c0490ca5
Qian <244901960@qq.com>
2021-11-02 09:34:57 +0800

    Init views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Home = () => import("../views/Home.vue")
+const Author = () => import("../views/Author.vue")
+const Institution = () => import("../views/Institution.vue")
+const Journal = () => import("../views/Journal.vue")
+const Search = () => import("../views/Search.vue")
+const Paper = () => import("../views/Paper.vue")
+const Admin = () => import("../views/Admin.vue")
+const About = () => import("../views/About.vue")
 
 Vue.use(VueRouter)
 
@@ -11,12 +19,39 @@ const routes = [
     component: Home
   },
   {
+    path: '/author',
+    name: 'Author',
+    component: Author
+  },
+  {
+    path: '/institution',
+    name: 'Institution',
+    component: Institution
+  },
+  {
+    path: '/journal',
+    name: 'Journal',
+    component: Journal
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/paper',
+    name: 'Paper',
+    component: Paper
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
   }
 ]
 
