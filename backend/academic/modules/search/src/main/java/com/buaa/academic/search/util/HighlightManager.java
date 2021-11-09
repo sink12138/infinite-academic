46abb690a0115827f984774f08b1d7d02008a673
YQ-zhishen <yangqian_1015@icloud.com>
2021-11-09 12:44:34 +0800

    Optimized HighlightManager; added reverse()

--- backend/academic/modules/search/src/main/java/com/buaa/academic/search/util/HighlightManager.java
+++ backend/academic/modules/search/src/main/java/com/buaa/academic/search/util/HighlightManager.java
@@ -3,41 +3,68 @@ package com.buaa.academic.search.util;
 public class HighlightManager {
 
     private final String[] tags = new String[2];
+    
+    private String[] sections;
+    
+    private int[] range;
+    
+    private String text;
+    
+    private boolean between;
 
     public HighlightManager(String preTag, String postTag) {
         tags[0] = preTag;
         tags[1] = postTag;
+    }
+    
+    public HighlightManager text(String text) {
+        this.text = text;
+        this.sections = text.split(String.format("(%s)|(%s)", tags[0], tags[1]));
+        return this;
     }
 
     public int length(String text) {
         return text.replaceAll(tags[0], "").replaceAll(tags[1], "").length();
     }
 
-    public String cut(String text, int endOffset) {
-        return cut(text, 0, endOffset);
+    public HighlightManager cut(int beginOffset, int endOffset) {
+        this.range = new int[] { beginOffset, endOffset };
+        return this;
     }
-
-    public String cut(String text, int beginOffset, int endOffset) {
+    
+    public HighlightManager cut(int endOffset) {
+        return cut(0, endOffset);
+    }
+    
+    public HighlightManager reverse() {
+        this.between = !this.between;
+        return this;
+    }
+    
+    public String process() {
+        if (this.text == null)
+            throw new NullPointerException("No input text");
+        if (this.range == null)
+            this.range = new int[] { 0, length(text) };
         StringBuilder builder = new StringBuilder();
-        String[] sections = text.split(String.format("(%s)|(%s)", tags[0], tags[1]));
-        boolean between = false;
+        boolean between = this.between;
         int index = 0;
         int offset = 0;
-        for (; offset < beginOffset && index < sections.length; between = !between, ++index) {
-            if (offset + sections[index].length() > beginOffset) {
-                sections[index] = sections[index].substring(beginOffset - offset);
-                offset = beginOffset;
+        for (; offset < this.range[0] && index < this.sections.length; between = !between, ++index) {
+            if (offset + sections[index].length() > this.range[0]) {
+                sections[index] = sections[index].substring(this.range[0] - offset);
+                offset = this.range[0];
                 break;
             }
             offset += sections[index].length();
         }
         if (index == sections.length)
-            throw new StringIndexOutOfBoundsException("begin " + beginOffset + ", end " + endOffset + ", length " + length(text));
-        if (between && offset < endOffset)
+            throw new StringIndexOutOfBoundsException("begin " + this.range[0] + ", end " + this.range[1] + ", length " + length(text));
+        if (between && offset < this.range[1])
             builder.append(tags[0]);
-        for (; offset < endOffset && index < sections.length; between = !between, ++index) {
-            if (offset + sections[index].length() > endOffset) {
-                builder.append(sections[index], 0, endOffset - offset);
+        for (; offset < this.range[1] && index < sections.length; between = !between, ++index) {
+            if (offset + sections[index].length() > this.range[1]) {
+                builder.append(sections[index], 0, this.range[1] - offset);
                 if (between)
                     builder.append(tags[1]);
                 break;
@@ -46,11 +73,11 @@ public class HighlightManager {
             offset += sections[index].length();
             if (between)
                 builder.append(tags[1]);
-            else if (offset < endOffset)
+            else if (offset < this.range[1])
                 builder.append(tags[0]);
         }
-        if (index == sections.length && offset < endOffset)
-            throw new StringIndexOutOfBoundsException("begin " + beginOffset + ", end " + endOffset + ", length " + length(text));
+        if (index == sections.length && offset < this.range[1])
+            throw new StringIndexOutOfBoundsException("begin " + this.range[0] + ", end " + this.range[1] + ", length " + length(text));
         return builder.toString();
     }
 
