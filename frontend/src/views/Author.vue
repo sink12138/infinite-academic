32e06f4dc22173279696ce2777c55b6ecb8cc1ec
c-hn <1961289751@qq.com>
2021-11-14 00:03:28 +0800

    anlytics

--- frontend/src/views/Author.vue
+++ frontend/src/views/Author.vue
@@ -2,15 +2,27 @@
   <div>
     <h1>Author</h1>
     <go-back></go-back>
+    <Analytics></Analytics>
   </div>
 </template>
 
 <script>
-import GoBack from '../components/GoBack.vue'
-
+import GoBack from '../components/GoBack.vue';
+import Analytics from "../components/Analytics.vue";
 export default {
-  components: { GoBack },
+    name: "Income",
+    components: {Analytics,GoBack},
+    data() {
+        return {
+        }
+    },
+    mounted(){
+      console.log(1);
+    },
+    methods:{
+    }
 }
+
 </script>
 
 <style>
