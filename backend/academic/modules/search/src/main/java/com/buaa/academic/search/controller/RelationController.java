dad8ded6478b52f488281a7147d57e44a089be32
YQ-zhishen <yangqian_1015@icloud.com>
2021-11-16 14:03:02 +0800

    [delete] RequestModel.java in module 'model'

--- backend/academic/modules/search/src/main/java/com/buaa/academic/search/controller/RelationController.java
+++ backend/academic/modules/search/src/main/java/com/buaa/academic/search/controller/RelationController.java
@@ -8,12 +8,14 @@ import com.buaa.academic.search.model.response.ScrollPage;
 import com.buaa.academic.search.model.vo.Relations;
 import com.buaa.academic.search.service.InfoService;
 import com.buaa.academic.search.service.RelationService;
-import com.buaa.academic.search.service.SearchService;
 import io.swagger.annotations.Api;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PositiveOrZero;
