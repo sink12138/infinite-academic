70f39d197144c1aeec58c8e72604851c68955a53
c-hn <1961289751@qq.com>
2021-11-21 00:04:21 +0800

    author

--- frontend/src/views/Author.vue
+++ frontend/src/views/Author.vue
@@ -1,30 +1,95 @@
 <template>
   <div>
-    <h1>Author</h1>
-    <Analytics></Analytics>
-    <BaseGoBack></BaseGoBack>
+    <v-card class="betop">
+      <BaseGoBack class="ml-6" style="float:left"></BaseGoBack>
+      <div class="ma-auto d-inline-flex">
+        <v-icon 
+          class="ml-5 text-h2"
+          color="indigo darken-4"
+        >mdi-home
+        </v-icon>
+        <div class="my-font my-5">
+          Author
+        </div>
+      </div>
+    </v-card>
+    <div class="whole">
+      <div class="message">
+        <h2>
+          {{name}}
+        </h2>
+        <h3>职位:{{position}}</h3>
+        <h3>科研方向:{{interests}}</h3>
+        <h3>邮箱:{{email}}</h3>
+        <h3>g指数:{{gIndex}}  h指数:{{hIndex}}</h3>
+        <h3>文章数量:{{paperNum}}  专利数量:{{patentNum}}</h3>
+        <h3>邮箱:{{email}}</h3>
+      </div>
+      <BaseAnalytics :data0=data></BaseAnalytics>
+    </div>
   </div>
+  
 </template>
 
 <script>
 import BaseGoBack from '../components/BaseGoBack.vue';
-import Analytics from "../components/Analytics.vue";
+import BaseAnalytics from "../components/BaseAnalytics.vue";
 export default {
     name: "Income",
-    components: {Analytics,BaseGoBack},
+    components: {BaseAnalytics,BaseGoBack},
     data() {
         return {
+          data:[3,2,4,3,2],
+          id:"GF_4ynwBF-Mu8unTG1hc",
+          name:"姓名",
+          position:"职位",
+          interests:"科研方向",
+          email:"1234@buaa.edu.cn",
+          gIndex:0,
+          hIndex:0,
+          paperNum:0,
+          patentNum:0,
+          currentInst:{
+            id:"000000",
+            name:"机构名"
+          },
+          institutions:[
+            {
+              id:"000000",
+              name:"机构名"
+            }
+          ],
         }
     },
     mounted(){
-      console.log(1);
+      this.getInfo()
     },
     methods:{
+      getInfo(){
+        this.$axios({
+          method: "get",
+          url: "api/search/info/researcher/"+this.id,
+          params: {
+            id: this.id
+          }
+        }).then(response => {
+          console.log(response.data)
+          this.name=response.data.name
+          this.position=response.data.position
+          this.interests=response.data.interests
+          this.email=response.data.email
+          this.hIndex=response.data.gIndex
+          this.paperNum=response.data.paperNum
+          this.patentNum=response.data.patentNum
+          this.currentInst=response.data.currentInst
+        }).catch(error => {
+          console.log(error)
+        })
+      }
     }
 }
 
 </script>
 
 <style>
-
 </style>
--- frontend/src/views/Institution.vue
+++ frontend/src/views/Institution.vue
@@ -1,17 +1,29 @@
 <template>
-  <v-card class="d-inline-flex align-center">
-    <BaseGoBack class="ml-6"></BaseGoBack>
-    <div class="ma-auto d-inline-flex">
-      <v-icon 
-        class="ml-5 text-h2"
-        color="indigo darken-4"
-      >mdi-home
-      </v-icon>
-      <div class="my-font my-5">
-        Institution
+  <div>
+    <v-card class="betop">
+      <BaseGoBack class="ml-6" style="float:left"></BaseGoBack>
+      <div class="ma-auto d-inline-flex">
+        <v-icon 
+          class="ml-5 text-h2"
+          color="indigo darken-4"
+        >mdi-home
+        </v-icon>
+        <div class="my-font my-5">
+          Institution
+        </div>
       </div>
+    </v-card>
+    <div class="whole">
+      <div class="message">
+        <h2>
+          {{name}}
+        </h2>
+        <h3>科研方向:{{interests}}</h3>
+        <h3>文章数量:{{paperNum}}  专利数量:{{patentNum}}</h3>
+      </div>
+      <BaseAnalytics :data0=data></BaseAnalytics>
     </div>
-  </v-card>
+  </div>
 </template>
 
 <script>
@@ -21,8 +33,19 @@ export default {
   components: {
     BaseGoBack,
   },
+  data() {
+    return {
+      data:[3,2,4,3,2],
+      id:"GF_4ynwBF-Mu8unTG1hc",
+      name:"机构名",
+      interests:"科研方向",
+      paperNum:0,
+      patentNum:0,
+    }
+  },
 }
 </script>
 
 <style>
+
 </style>
