4c22e686864d3ddb890b07b12b102c40fc28c4de
YQ-zhishen <yangqian_1015@icloud.com>
2021-11-24 16:40:43 +0800

    [fix] bug in API doc

--- backend/academic/modules/search/src/main/java/com/buaa/academic/search/model/response/SmartPage.java
+++ backend/academic/modules/search/src/main/java/com/buaa/academic/search/model/response/SmartPage.java
@@ -2,17 +2,16 @@ package com.buaa.academic.search.model.response;
 
 import com.buaa.academic.document.entity.Paper;
 import com.buaa.academic.document.entity.item.PaperItem;
-import com.buaa.academic.search.util.HighlightManager;
 import com.buaa.academic.search.util.HitsReducer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
-import org.springframework.data.elasticsearch.core.SearchHit;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.elasticsearch.core.SearchHits;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @Setter
@@ -21,7 +20,7 @@ import java.util.Map;
 @ApiModel(description = "智能搜索结果模型")
 public class SmartPage extends HitPage<PaperItem> {
 
-    @ApiModelProperty(value = "检测到并推荐的实体类型，若为null表示没有推荐", allowableValues = "可能的值为researcher, institution, journal")
+    @ApiModelProperty(value = "检测到并推荐的实体类型，若为null表示没有推荐。可能的值为researcher/institution/journal")
     protected String detection;
 
     @ApiModelProperty(value = "推荐实体列表，最多6个")
