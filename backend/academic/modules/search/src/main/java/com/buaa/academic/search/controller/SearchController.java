2fc183158995e9b8a05a67967ae381f5cbdec403
YQ-zhishen <yangqian_1015@icloud.com>
2021-11-25 17:46:18 +0800

    [finish] All APIs, docs in SearchController, and related methods & classes

--- backend/academic/modules/search/src/main/java/com/buaa/academic/search/controller/SearchController.java
+++ backend/academic/modules/search/src/main/java/com/buaa/academic/search/controller/SearchController.java
@@ -1,13 +1,7 @@
 package com.buaa.academic.search.controller;
 
-import com.buaa.academic.document.entity.Institution;
-import com.buaa.academic.document.entity.Journal;
-import com.buaa.academic.document.entity.Paper;
-import com.buaa.academic.document.entity.Researcher;
-import com.buaa.academic.document.entity.item.InstitutionItem;
-import com.buaa.academic.document.entity.item.JournalItem;
-import com.buaa.academic.document.entity.item.PaperItem;
-import com.buaa.academic.document.entity.item.ResearcherItem;
+import com.buaa.academic.document.entity.*;
+import com.buaa.academic.document.entity.item.*;
 import com.buaa.academic.model.exception.ExceptionType;
 import com.buaa.academic.model.web.Result;
 import com.buaa.academic.search.dao.InstitutionRepository;
@@ -30,6 +24,7 @@ import io.swagger.annotations.ApiOperation;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.SortBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
@@ -208,7 +203,7 @@ public class SearchController {
                     <b>type</b> - 论文本身的类别，如学位论文、期刊论文等，不允许开启模糊搜索和语种关联，且不允许与其他scope共存</br>
                     <b>authors.name</b> - 论文作者的姓名，不允许开启模糊搜索和语种关联，且不允许与其他scope共存</br>
                     <b>institutions.name</b> - 发表论文的机构名，不允许与其他scope共存</br>
-                    <b>journal.title</b> - 刊登论文的期刊名，不允许开启语种关联，且不允许与其他scope共存</br>
+                    <b>journal.title</b> - 刊登论文的期刊名，不允许与其他scope共存</br>
                     
                     允许添加filter的字段：year (below, above, range, equal), citationNum (above)
                     
@@ -227,9 +222,9 @@ public class SearchController {
             return result.withFailure("条件复合不能超过" + maxDepth + "层");
 
         /* All fields that are allowed to be contained in the scope */
-        Set<String> allAllowedScopes = Set.of(new String[]{ "title", "abstract", "keywords", "subjects", "topics", "type", "authors.name", "institutions.name", "journal.title" });
+        Set<String> allAllowedScopes = Set.of("title", "abstract", "keywords", "subjects", "topics", "type", "authors.name", "institutions.name", "journal.title");
         /* Fields that are not allowed to appear with any other field */
-        Set<String> soloFieldScopes = Set.of(new String[] { "type", "authors.name", "institutions.name", "journal.title" });
+        Set<String> soloFieldScopes = Set.of("type", "authors.name", "institutions.name", "journal.title");
 
         // Hierarchical iteration
         Queue<Condition> checkQueue = new LinkedList<>(conditions);
@@ -251,10 +246,6 @@ public class SearchController {
             if (scope.size() > 1) {
                 if (!Collections.disjoint(scope, soloFieldScopes))
                     return result.withFailure("非法的关键词范围组合");
-            }
-            if (scope.contains("journal.title")) {
-                if (condition.isTranslated())
-                    return result.withFailure("期刊标题不可开启语种关联");
             }
             if (scope.contains("type")) {
                 if (condition.isFuzzy())
@@ -298,15 +289,11 @@ public class SearchController {
             }
         }
 
-        // Conditions
+        // Prepare search params
         QueryBuilder query = searchService.buildQuery(conditions);
-        // Filters
         QueryBuilder filter = searchService.buildFilter(filters);
-        // Sort
         SortBuilder<?> sort = searchService.buildSort(srt);
-        // Highlight
         HighlightBuilder hlt = searchService.buildHighlight("title", "abstract", "keywords");
-        // Page
         Pageable page = PageRequest.of(searchRequest.getPage(), searchRequest.getSize());
 
         // Run search
@@ -314,6 +301,315 @@ public class SearchController {
 
         // Items & statistics
         hitPage.setItems(HitsReducer.reducePaperHits(hits, preTag, postTag));
+        hitPage.setStatistics(hits.getTotalHits(), searchRequest.getPage(), searchRequest.getSize());
+
+        // Time cost
+        hitPage.setTimeCost(System.currentTimeMillis() - start);
+
+        return result.withData(hitPage);
+    }
+
+    @PostMapping("/researcher")
+    @ApiOperation(
+            value = "科研人员检索",
+            notes = """
+                    使用多个条件或嵌套条件搜索科研人员。
+                    
+                    条件中的scope参数允许的所有值的含义与限制规则如下：</br>
+                    <b>name</b> - 科研人员姓名，不允许开启模糊搜索和语种关联，且不允许与其他scope共存</br>
+                    <b>interests</b> - 科研人员研究方向，不允许与其他scope共存</br>
+                    <b>currentInst.name</b> - 科研人员所属的机构名</br>
+                    <b>institutions.name</b> - 科研人员的合作机构名</br>
+                    
+                    允许添加filter的字段：hIndex, gIndex, paperNum, patentNum, citationNum（type均只能是above）
+                    
+                    允许设置sort的字段：hIndex.desc, gIndex.desc, paperNum.desc, patentNum.desc, citationNum.desc""")
+    public Result<HitPage<ResearcherItem>> searchResearcher(@RequestBody @Valid SearchRequest searchRequest) {
+        long start = System.currentTimeMillis();
+        Result<HitPage<ResearcherItem>> result = new Result<>();
+        HitPage<ResearcherItem> hitPage = new HitPage<>();
+
+        List<Condition> conditions = searchRequest.getConditions();
+        List<Filter> filters = searchRequest.getFilters();
+        String srt = searchRequest.getSort(); // Sort
+
+        // Condition tree depth check
+        if (!validateDepth(conditions, maxDepth))
+            return result.withFailure("条件复合不能超过" + maxDepth + "层");
+
+        /* All fields that are allowed to be contained in the scope */
+        Set<String> allAllowedScopes = Set.of("name", "interests", "currentInst.name", "institutions.name");
+
+        // Hierarchical iteration
+        Queue<Condition> checkQueue = new LinkedList<>(conditions);
+        while (!checkQueue.isEmpty()) {
+            Condition condition = checkQueue.remove();
+            if (condition.isCompound()) {
+                checkQueue.addAll(condition.getSubConditions());
+                continue;
+            }
+            // Strip the keyword
+            condition.setKeyword(StringUtils.strip(condition.getKeyword(), 64));
+
+            // Scope validation
+            Set<String> scope = condition.getScope();
+            // Single scope constraints check
+            if (!validateScope(scope, allAllowedScopes))
+                return result.withFailure(ExceptionType.INVALID_PARAM);
+            // Multiple scopes constraints check
+            if (scope.size() > 1) {
+                if (scope.contains("name") && scope.contains("interests"))
+                    return result.withFailure("非法的关键词范围组合");
+            }
+            if (scope.contains("name")) {
+                if (condition.isFuzzy())
+                    return result.withFailure("学者姓名不可开启模糊搜索");
+                if (condition.isTranslated())
+                    return result.withFailure("学者姓名不可开启语种关联");
+            }
+        }
+
+        // Param check for filters
+        for (Filter filter : filters) {
+            if (!filter.getType().equals("above"))
+                return result.withFailure(ExceptionType.INVALID_PARAM);
+            switch (filter.getAttr()) {
+                case "hIndex", "gIndex", "paperNum", "patentNum", "citationNum" -> {}
+                default -> {
+                    return result.withFailure(ExceptionType.INVALID_PARAM);
+                }
+            }
+        }
+
+        // Sort check
+        if (srt != null) {
+            switch (srt) {
+                case "hIndex.desc", "gIndex.desc", "paperNum.desc", "patentNum.desc", "citationNum.desc" -> {}
+                default -> {
+                    return result.withFailure(ExceptionType.INVALID_PARAM);
+                }
+            }
+        }
+
+        // Prepare search params
+        QueryBuilder query = searchService.buildQuery(conditions);
+        QueryBuilder filter = searchService.buildFilter(filters);
+        SortBuilder<?> sort = searchService.buildSort(srt);
+        HighlightBuilder hlt = searchService.buildHighlight("name", "interests");
+        Pageable page = PageRequest.of(searchRequest.getPage(), searchRequest.getSize());
+
+        // Run search
+        SearchHits<Researcher> hits = searchService.advancedSearch(Researcher.class, query, filter, sort, hlt, page);
+
+        // Items & statistics
+        hitPage.setItems(HitsReducer.reduceResearcherHits(hits));
+        hitPage.setStatistics(hits.getTotalHits(), searchRequest.getPage(), searchRequest.getSize());
+
+        // Time cost
+        hitPage.setTimeCost(System.currentTimeMillis() - start);
+
+        return result.withData(hitPage);
+    }
+
+    @PostMapping("/journal")
+    @ApiOperation(
+            value = "学术期刊检索",
+            notes = """
+                    使用多个条件或嵌套条件搜索学术期刊。
+                    
+                    条件中的scope参数允许的所有值的含义与限制规则如下：</br>
+                    <b>title</b> - 学术期刊标题</br>
+                    
+                    不支持添加任何filter。
+                    
+                    不支持设置任何sort。""")
+    public Result<HitPage<JournalItem>> searchJournal(@RequestBody @Valid SearchRequest searchRequest) {
+        long start = System.currentTimeMillis();
+        Result<HitPage<JournalItem>> result = new Result<>();
+        HitPage<JournalItem> hitPage = new HitPage<>();
+
+        List<Condition> conditions = searchRequest.getConditions();
+
+        // Condition tree depth check
+        if (!validateDepth(conditions, maxDepth))
+            return result.withFailure("条件复合不能超过" + maxDepth + "层");
+
+        // Hierarchical iteration
+        Queue<Condition> checkQueue = new LinkedList<>(conditions);
+        while (!checkQueue.isEmpty()) {
+            Condition condition = checkQueue.remove();
+            if (condition.isCompound()) {
+                checkQueue.addAll(condition.getSubConditions());
+                continue;
+            }
+            // Strip the keyword
+            condition.setKeyword(StringUtils.strip(condition.getKeyword(), 64));
+
+            // Scope validation
+            Set<String> scope = condition.getScope();
+            if (!scope.contains("title") || scope.size() > 1)
+                return result.withFailure(ExceptionType.INVALID_PARAM);
+        }
+
+        // Prepare search params
+        QueryBuilder query = searchService.buildQuery(conditions);
+        SortBuilder<?> sort = SortBuilders.scoreSort();
+        HighlightBuilder hlt = searchService.buildHighlight("title");
+        Pageable page = PageRequest.of(searchRequest.getPage(), searchRequest.getSize());
+
+        // Run search
+        SearchHits<Journal> hits = searchService.advancedSearch(Journal.class, query, null, sort, hlt, page);
+
+        // Items & statistics
+        hitPage.setItems(HitsReducer.reduceJournalHits(hits));
+        hitPage.setStatistics(hits.getTotalHits(), searchRequest.getPage(), searchRequest.getSize());
+
+        // Time cost
+        hitPage.setTimeCost(System.currentTimeMillis() - start);
+
+        return result.withData(hitPage);
+    }
+
+    @PostMapping("/institution")
+    @ApiOperation(
+            value = "学术机构检索",
+            notes = """
+                    使用多个条件或嵌套条件搜索学术机构。
+                    
+                    条件中的scope参数允许的所有值的含义与限制规则如下：</br>
+                    <b>name</b> - 科研机构名称</br>
+                    
+                    不支持添加任何filter。
+                    
+                    不支持设置任何sort。""")
+    public Result<HitPage<InstitutionItem>> searchInstitution(@RequestBody @Valid SearchRequest searchRequest) {
+        long start = System.currentTimeMillis();
+        Result<HitPage<InstitutionItem>> result = new Result<>();
+        HitPage<InstitutionItem> hitPage = new HitPage<>();
+
+        List<Condition> conditions = searchRequest.getConditions();
+
+        // Condition tree depth check
+        if (!validateDepth(conditions, maxDepth))
+            return result.withFailure("条件复合不能超过" + maxDepth + "层");
+
+        // Hierarchical iteration
+        Queue<Condition> checkQueue = new LinkedList<>(conditions);
+        while (!checkQueue.isEmpty()) {
+            Condition condition = checkQueue.remove();
+            if (condition.isCompound()) {
+                checkQueue.addAll(condition.getSubConditions());
+                continue;
+            }
+            // Strip the keyword
+            condition.setKeyword(StringUtils.strip(condition.getKeyword(), 64));
+
+            // Scope validation
+            Set<String> scope = condition.getScope();
+            if (!scope.contains("name") || scope.size() > 1)
+                return result.withFailure(ExceptionType.INVALID_PARAM);
+        }
+
+        // Prepare search params
+        QueryBuilder query = searchService.buildQuery(conditions);
+        SortBuilder<?> sort = SortBuilders.scoreSort();
+        HighlightBuilder hlt = searchService.buildHighlight("name");
+        Pageable page = PageRequest.of(searchRequest.getPage(), searchRequest.getSize());
+
+        // Run search
+        SearchHits<Institution> hits = searchService.advancedSearch(Institution.class, query, null, sort, hlt, page);
+
+        // Items & statistics
+        hitPage.setItems(HitsReducer.reduceInstitutionHits(hits));
+        hitPage.setStatistics(hits.getTotalHits(), searchRequest.getPage(), searchRequest.getSize());
+
+        // Time cost
+        hitPage.setTimeCost(System.currentTimeMillis() - start);
+
+        return result.withData(hitPage);
+    }
+
+    @PostMapping("/patent")
+    @ApiOperation(
+            value = "专利检索",
+            notes = """
+                    使用多个条件或嵌套条件搜索专利。
+                    
+                    条件中的scope参数允许的所有值的含义与限制规则如下：</br>
+                    <b>title</b> - 专利标题</br>
+                    <b>abstract</b> - 专利摘要</br>
+                    <b>applicant</b> - 专利申请人，不允许与其他scope共存</br>
+                    <b>inventors.name</b> - 专利发明人，不允许开启模糊搜索和语种关联，且不允许与其他scope共存</br>
+
+                    不支持添加任何filter。
+                    
+                    允许设置sort的字段：fillingDate.desc, fillingDate.asc（即申请日期正序或倒序）""")
+    public Result<HitPage<PatentItem>> searchPatent(@RequestBody @Valid SearchRequest searchRequest) {
+        long start = System.currentTimeMillis();
+        Result<HitPage<PatentItem>> result = new Result<>();
+        HitPage<PatentItem> hitPage = new HitPage<>();
+
+        List<Condition> conditions = searchRequest.getConditions();
+        String srt = searchRequest.getSort();
+
+        // Condition tree depth check
+        if (!validateDepth(conditions, maxDepth))
+            return result.withFailure("条件复合不能超过" + maxDepth + "层");
+        /* All fields that are allowed to be contained in the scope */
+        Set<String> allAllowedScopes = Set.of("title", "abstract", "applicant", "inventors.name");
+        /* Fields that are not allowed to appear with any other field */
+        Set<String> soloFieldScopes = Set.of("applicant", "inventors.name");
+
+        // Hierarchical iteration
+        Queue<Condition> checkQueue = new LinkedList<>(conditions);
+        while (!checkQueue.isEmpty()) {
+            Condition condition = checkQueue.remove();
+            if (condition.isCompound()) {
+                checkQueue.addAll(condition.getSubConditions());
+                continue;
+            }
+            // Strip the keyword
+            condition.setKeyword(StringUtils.strip(condition.getKeyword(), 64));
+
+            // Scope validation
+            Set<String> scope = condition.getScope();
+            // Single scope constraints check
+            if (!validateScope(scope, allAllowedScopes))
+                return result.withFailure(ExceptionType.INVALID_PARAM);
+            // Multiple scopes constraints check
+            if (scope.size() > 1) {
+                if (!Collections.disjoint(scope, soloFieldScopes))
+                    return result.withFailure("非法的关键词范围组合");
+            }
+            if (scope.contains("inventors.name")) {
+                if (condition.isFuzzy())
+                    return result.withFailure("发明人姓名不可开启模糊搜索");
+                if (condition.isTranslated())
+                    return result.withFailure("发明人姓名不可开启语种关联");
+            }
+        }
+
+        // Sort check
+        if (srt != null) {
+            switch (srt) {
+                case "filingDate.asc", "filingDate.desc" -> {}
+                default -> {
+                    return result.withFailure(ExceptionType.INVALID_PARAM);
+                }
+            }
+        }
+
+        // Prepare search params
+        QueryBuilder query = searchService.buildQuery(conditions);
+        SortBuilder<?> sort = searchService.buildSort(srt);
+        HighlightBuilder hlt = searchService.buildHighlight("title", "applicant");
+        Pageable page = PageRequest.of(searchRequest.getPage(), searchRequest.getSize());
+
+        // Run search
+        SearchHits<Patent> hits = searchService.advancedSearch(Patent.class, query, null, sort, hlt, page);
+
+        // Items & statistics
+        hitPage.setItems(HitsReducer.reducePatentHits(hits, preTag, postTag));
         hitPage.setStatistics(hits.getTotalHits(), searchRequest.getPage(), searchRequest.getSize());
 
         // Time cost
--- backend/academic/modules/search/src/main/java/com/buaa/academic/search/util/HitsReducer.java
+++ backend/academic/modules/search/src/main/java/com/buaa/academic/search/util/HitsReducer.java
@@ -1,7 +1,7 @@
 package com.buaa.academic.search.util;
 
-import com.buaa.academic.document.entity.Paper;
-import com.buaa.academic.document.entity.item.PaperItem;
+import com.buaa.academic.document.entity.*;
+import com.buaa.academic.document.entity.item.*;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 
@@ -31,6 +31,65 @@ public class HitsReducer {
                 if (manager.text(paperAbstract).length() > 128)
                     paperAbstract = manager.cut(128).process() + "...";
                 item.setPaperAbstract(paperAbstract);
+            }
+            result.add(item);
+        }
+        return result;
+    }
+
+    public static List<ResearcherItem> reduceResearcherHits(SearchHits<Researcher> hits) {
+        List<ResearcherItem> result = new ArrayList<>();
+        for (SearchHit<Researcher> hit : hits) {
+            ResearcherItem item = hit.getContent().reduce();
+            Map<String, List<String>> hlt = hit.getHighlightFields();
+            if (hlt.containsKey("name")) {
+                item.setName(hlt.get("name").get(0));
+            }
+            if (hlt.containsKey("interests")) {
+                item.setInterests(hlt.get("interests"));
+            }
+            result.add(item);
+        }
+        return result;
+    }
+
+    public static List<JournalItem> reduceJournalHits(SearchHits<Journal> hits) {
+        List<JournalItem> result = new ArrayList<>();
+        for (SearchHit<Journal> hit : hits) {
+            JournalItem item = hit.getContent().reduce();
+            Map<String, List<String>> hlt = hit.getHighlightFields();
+            if (hlt.containsKey("title")) {
+                item.setTitle(hlt.get("title").get(0));
+            }
+            result.add(item);
+        }
+        return result;
+    }
+
+    public static List<InstitutionItem> reduceInstitutionHits(SearchHits<Institution> hits) {
+        List<InstitutionItem> result = new ArrayList<>();
+        for (SearchHit<Institution> hit : hits) {
+            InstitutionItem item = hit.getContent().reduce();
+            Map<String, List<String>> hlt = hit.getHighlightFields();
+            if (hlt.containsKey("name")) {
+                item.setName(hlt.get("name").get(0));
+            }
+            result.add(item);
+        }
+        return result;
+    }
+
+    public static List<PatentItem> reducePatentHits(SearchHits<Patent> hits, String preTag, String postTag) {
+        List<PatentItem> result = new ArrayList<>();
+        HighlightManager manager = new HighlightManager(preTag, postTag);
+        for (SearchHit<Patent> hit : hits) {
+            PatentItem item = hit.getContent().reduce();
+            Map<String, List<String>> hlt = hit.getHighlightFields();
+            if (hlt.containsKey("title")) {
+                String title = hlt.get("title").get(0);
+                if (manager.text(title).length() > 64)
+                    title = manager.cut(64).process() + "...";
+                item.setTitle(title);
             }
             result.add(item);
         }
