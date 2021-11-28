b0fa32e2627fdd6404ab05de1dd6253916356114
hjgg123 <1642970634@qq.com>
2021-11-28 20:40:24 +0800

    searchResult分类展示

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -14,19 +14,76 @@
       </div>
       <div class="result">
         <v-row v-for="item in results" :key="item">
-          <v-card class="card" max-width="400">
-            <v-card-title>{{ item.title }}</v-card-title>
-            <v-card-subtitle>{{ item.journal }}</v-card-subtitle>
-            <v-card-text>
-              <div>摘要：</div>
-              <div>{{ item.abstract }}</div>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn color="blue" text> 转发 </v-btn>
-              <v-btn color="blue" text> 引用 </v-btn>
-            </v-card-actions>
-          </v-card>
-          <v-spacer></v-spacer>
+          <div v-if="item.authors!=null">
+            <v-card class="card" max-width="400">
+              <v-card-title>{{ item.title }}</v-card-title>
+              <v-card-subtitle>{{ item.journal }}</v-card-subtitle>
+              <v-card-text>
+                <div>作者：</div>
+                <div v-for="author in item.authors" :key="author">{{ author.name }}</div>
+              </v-card-text>
+              <v-card-text>
+                <div>摘要：</div>
+                <div>{{ item.abstract }}</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="blue" text> 转发 </v-btn>
+                <v-btn color="blue" text> 引用 </v-btn>
+              </v-card-actions>
+            </v-card>
+            <v-spacer></v-spacer>
+          </div>
+          <div v-else-if="item.interests!=null">
+            <v-card class="card" max-width="400">
+              <v-card-title>{{ item.title }}</v-card-title>
+              <v-card-subtitle>{{ item.journal }}</v-card-subtitle>
+              <v-card-text>
+                <div>摘要：</div>
+                <div>{{ item.abstract }}</div>
+              </v-card-text>
+              <v-card-text>
+                <div>研究方向:</div>
+                <div>{{ item.interests }}</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="blue" text> 转发 </v-btn>
+                <v-btn color="blue" text> 引用 </v-btn>
+              </v-card-actions>
+            </v-card>
+            <v-spacer></v-spacer>
+          </div>
+          <div v-else-if="item.applicant!=null">
+            <v-card class="card" max-width="400">
+              <v-card-title>{{ item.title }}</v-card-title>
+              <v-card-subtitle>{{ item.journal }}</v-card-subtitle>
+              <v-card-subtitle>申请人：{{ item.applicant }}</v-card-subtitle>
+              <v-card-text>
+                <div>摘要：</div>
+                <div>{{ item.abstract }}</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="blue" text> 转发 </v-btn>
+                <v-btn color="blue" text> 引用 </v-btn>
+              </v-card-actions>
+            </v-card>
+            <v-spacer></v-spacer>
+          </div>
+          <div v-else>
+            <v-card class="card" max-width="400">
+              <v-card-title>{{ item.title }}</v-card-title>
+              <v-card-subtitle>{{ item.journal }}</v-card-subtitle>
+              <v-card-subtitle>name|:{{ item.name }}</v-card-subtitle>
+              <v-card-text>
+                <div>摘要：</div>
+                <div>{{ item.abstract }}</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="blue" text> 转发 </v-btn>
+                <v-btn color="blue" text> 引用 </v-btn>
+              </v-card-actions>
+            </v-card>
+            <v-spacer></v-spacer>
+          </div>
         </v-row>
       </div>
     </div>
@@ -48,9 +105,10 @@ export default {
     return {
       filters:{},
       results: [
-        { title: "aaa", journal: "b", abstract: "114514" },
-        { title: "aaa", journal: "b", abstract: "114514" },
-        { title: "aaa", journal: "b", abstract: "114514" },
+        { title: "aaa", journal: "b", abstract: "114514" , authors: [{name:"name1"},{name:"name2"}]},
+        { title: "aaa", journal: "b", abstract: "114514" , interests: "软件工程"},
+        { title: "aaa", journal: "b", abstract: "114514" , applicant: "谭火彬"},
+        { title: "aaa", journal: "b", abstract: "114514" , name: "name"},
       ],
       data:{},
     };
