833ae747a20e5292b130f3d74d1055b5d1f04801
YQ-zhishen <yangqian_1015@icloud.com>
2021-11-30 21:00:44 +0800

    [add] routing & checking for analysis service

--- backend/academic/modules/gateway/src/main/java/com/buaa/academic/gateway/filter/AuthorityFilter.java
+++ backend/academic/modules/gateway/src/main/java/com/buaa/academic/gateway/filter/AuthorityFilter.java
@@ -5,6 +5,7 @@ import com.buaa.academic.model.exception.AcademicException;
 import com.buaa.academic.model.exception.ExceptionType;
 import com.buaa.academic.model.security.Authority;
 import com.buaa.academic.model.web.Result;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -21,61 +22,109 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Base64;
 import java.util.List;
 
 @Component
 public class AuthorityFilter implements GlobalFilter, Ordered {
 
+    private static final Base64.Encoder encoder = Base64.getEncoder();
+
+    private static String adminAuth;
+
     @Resource
     private RedisTemplate<String, Authority> redisTemplate;
+
+    @Value("${auth.max-inactive-interval}")
+    private long maxInactiveInterval;
+
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
+    @PostConstruct
+    public void init() {
+        if (adminAuth == null) {
+            adminAuth = encoder.encodeToString((adminUsername + '@' + adminPassword).getBytes(StandardCharsets.UTF_8));
+        }
+    }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         RequestPath path = request.getPath();
-        if (path.value().startsWith("/search") || path.value().matches("^/(?!/).*/v2/api-docs$"))
+        String pathValue = path.value();
+
+        /* Frequently requested non-auth APIs */
+        if (pathValue.startsWith("/search") || pathValue.matches("^/analysis/(?!update).*"))
             return chain.filter(exchange);
+
+        /* API docs */
+        if (pathValue.matches("^/(?!/).*/v2/api-docs$"))
+            return chain.filter(exchange);
+
         List<PathContainer.Element> urlElements = path.elements();
         if (urlElements.size() < 2) {
             exchange.getResponse().setStatusCode(HttpStatus.NOT_FOUND);
             return exchange.getResponse().setComplete();
         }
+
+        /* Retrieve & validate cookie value */
         HttpCookie cookie = request.getCookies().getFirst("TOKEN");
         String token = cookie == null ? null : cookie.getValue();
         Authority authority = token == null ? null : redisTemplate.opsForValue().get(token);
-        if (authority != null)
-            redisTemplate.expire(token, Duration.ofDays(1));
+        if (authority != null) {
+            /* For requests without admin authority, reset their inactive interval */
+            if (!authority.isAdmin()) {
+                redisTemplate.expire(token, Duration.ofSeconds(maxInactiveInterval));
+            }
+        }
         else
             authority = new Authority();
-        String headerId;
+
+        String authHeader;
         try {
             String primaryUrl = urlElements.get(1).value();
             String secondaryUrl = urlElements.size() < 4 ? "" : urlElements.get(3).value();
+
+            /* Switch url paths */
             switch (primaryUrl) {
+                case "analysis" -> {
+                    // When reaching this, request must have admin auth
+                    if (!authority.isAdmin())
+                        throw new AcademicException(ExceptionType.UNAUTHORIZED);
+                    authHeader = adminAuth;
+                }
                 case "account" -> {
                     switch (secondaryUrl) {
-                        case "register", "login", "verify", "forget", "echo":
+                        case "register", "login", "verify", "forget", "echo" -> {
+                            // These requests do not need authority checks here
                             return chain.filter(exchange);
-                        default: {
+                        }
+                        default -> {
                             if (authority.getUserId() == null)
                                 throw new AcademicException(ExceptionType.UNAUTHORIZED);
-                            headerId = authority.getUserId();
+                            authHeader = authority.getUserId();
                         }
                     }
                 }
                 case "researcher" -> {
                     if ("certify".equals(secondaryUrl)) {
+                        // These requests need userId instead of researcherId
                         if (authority.getUserId() == null)
                             throw new AcademicException(ExceptionType.UNAUTHORIZED);
-                        headerId = authority.getUserId();
+                        authHeader = authority.getUserId();
                     }
                     else {
                         if (authority.getResearcherId() == null)
                             throw new AcademicException(ExceptionType.UNAUTHORIZED);
-                        headerId = authority.getResearcherId();
+                        authHeader = authority.getResearcherId();
                     }
                 }
                 case "admin" -> {
@@ -89,15 +138,18 @@ public class AuthorityFilter implements GlobalFilter, Ordered {
             }
         }
         catch (AcademicException exception) {
+            /* Re-write response body for unauthorized requests */
             JSONObject jsonObject = (JSONObject) JSONObject.toJSON(new Result<Void>().withFailure(exception.getMessage()));
             ServerHttpResponse response = exchange.getResponse();
             response.getHeaders().add("Content-Type", MediaType.APPLICATION_JSON.toString());
             DataBuffer buffer = response.bufferFactory().wrap(jsonObject.toJSONString().getBytes(StandardCharsets.UTF_8));
             return response.writeWith(Mono.just(buffer));
         }
+
+        /* Add authority header */
         ServerHttpRequest newRequest = request.mutate()
-                .headers(header -> header.remove("Role-ID"))
-                .header("Role-ID", headerId).build();
+                .headers(header -> header.remove("Auth"))
+                .header("Auth", authHeader).build();
         return chain.filter(exchange.mutate().request(newRequest).build());
     }
 
