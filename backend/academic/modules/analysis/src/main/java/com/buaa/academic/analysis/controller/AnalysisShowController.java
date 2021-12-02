729728da3a99c5802af596e1082f4f621e123239
Henry <52558285+MiserableFaithAI@users.noreply.github.com>
2021-12-02 16:41:04 +0800

    fix result type bug of AnalysisShowController

--- backend/academic/modules/analysis/src/main/java/com/buaa/academic/analysis/controller/AnalysisShowController.java
+++ backend/academic/modules/analysis/src/main/java/com/buaa/academic/analysis/controller/AnalysisShowController.java
@@ -1,14 +1,13 @@
 package com.buaa.academic.analysis.controller;
 
-import com.buaa.academic.analysis.model.Cooperation;
-import com.buaa.academic.analysis.model.SearchAggregation;
-import com.buaa.academic.analysis.model.WordFrequency;
+import com.buaa.academic.analysis.model.*;
 import com.buaa.academic.analysis.repository.*;
 import com.buaa.academic.analysis.service.AnalysisShowService;
 import com.buaa.academic.document.statistic.Association;
 import com.buaa.academic.document.statistic.DataPerYear;
 import com.buaa.academic.document.statistic.Subject;
 import com.buaa.academic.document.statistic.Topic;
+import com.buaa.academic.model.exception.ExceptionType;
 import com.buaa.academic.model.web.Result;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,103 +38,111 @@ public class AnalysisShowController {
 
     @ApiOperation(value = "搜索结果聚类分析")
     @GetMapping("/searchAgg")
-    public Result<Object> searchAggregation() {
+    public Result<SearchAggregation> searchAggregation() {
+        Result<SearchAggregation> result = new Result<>();
         HttpSession session = request.getSession();
         SearchAggregation searchAggregation = analysisShowService.searchAggregating(session);
         if (searchAggregation == null)
-            return new Result<>().withFailure("该搜索不支持结果过滤");
-        return new Result<>().withData(searchAggregation);
+            return result.withFailure("该搜索不支持结果过滤");
+        return result.withData(searchAggregation);
     }
 
     @ApiOperation(value = "话题主页", notes = "包括关联分析、发表统计分析")
     @GetMapping("/topic")
-    public Result<Object> showTopic(@RequestParam(value = "name") @NotNull String name) {
+    public Result<Topic> showTopic(@RequestParam(value = "name") @NotNull String name) {
+        Result<Topic> result = new Result<>();
         Topic topic = TopicDao.getTopicByName(name, template);
         if (topic == null)
-            return new Result<>().withFailure("话题不存在");
-        return new Result<>().withData(topic);
+            return result.withFailure("话题不存在");
+        return result.withData(topic);
     }
 
     @ApiOperation(value = "学科主页", notes = "包括关联分析、发表统计分析")
     @GetMapping("/subject")
-    public Result<Object> showSubject(@RequestParam(value = "name") @NotNull String name) {
+    public Result<Subject> showSubject(@RequestParam(value = "name") @NotNull String name) {
+        Result<Subject> result = new Result<>();
         Subject subject = SubjectDao.getSubjectByName(name, template);
         if (subject == null)
-            return new Result<>().withFailure("学科不存在");
-        return new Result<>().withData(subject);
+            return result.withFailure(ExceptionType.NOT_FOUND);
+        return result.withData(subject);
     }
 
     @ApiOperation(value = "热门学科", notes = "获取当前热点最高的前十个学科")
     @GetMapping("/hotTopics")
-    public Result<Object> hotTopics() {
-       return new Result<>().withData(analysisShowService.getHotTopics());
+    public Result<ArrayList<SimpleTopic>> hotTopics() {
+       return new Result<ArrayList<SimpleTopic>>().withData(analysisShowService.getHotTopics());
     }
 
     @ApiOperation(value = "热门话题", notes = "获取当前热点最高的前十个话题")
     @GetMapping("/hotSubjects")
-    public Result<Object> hotSubjects() {
-        return new Result<>().withData(analysisShowService.getHotSubjects());
+    public Result<ArrayList<SimpleSubject>> hotSubjects() {
+        return new Result<ArrayList<SimpleSubject>>().withData(analysisShowService.getHotSubjects());
     }
 
     @ApiOperation(value = "话题关联分析", notes = "用于搜索时相关推荐")
     @GetMapping("/topicAssociation")
-    public Result<Object> topicAssociationAnalysis(@RequestParam(value = "name") @NotNull String name) {
+    public Result<List<Association>> topicAssociationAnalysis(@RequestParam(value = "name") @NotNull String name) {
+        Result<List<Association>> result = new Result<>();
         Topic topic = TopicDao.getTopicByName(name, template);
         if (topic == null)
-            return new Result<>().withFailure("话题不存在");
+            return result.withFailure(ExceptionType.NOT_FOUND);
         List<Association> associations = topic.getAssociationTopics();
-        return new Result<>().withData(associations);
+        return result.withData(associations);
     }
 
     @ApiOperation(value = "学科关联分析", notes = "用于搜索时相关推荐")
     @GetMapping("/subjectAssociation")
-    public Result<Object> subjectAssociationAnalysis(@RequestParam(value = "name") @NotNull String name) {
+    public Result<List<Association>> subjectAssociationAnalysis(@RequestParam(value = "name") @NotNull String name) {
+        Result<List<Association>> result = new Result<>();
         Subject subject = SubjectDao.getSubjectByName(name, template);
         if (subject == null)
-            return new Result<>().withFailure("学科不存在");
+            return result.withFailure(ExceptionType.NOT_FOUND);
         List<Association> associations = subject.getAssociationSubjects();
-        return new Result<>().withData(associations);
+        return result.withData(associations);
     }
 
     @ApiOperation(value = "每年发文统计", notes = "用于机构、学者、期刊的发文统计图，" +
             "type类型可为researcher、institution和journal，id即为分析目标的id")
     @GetMapping("/pubStaticByYear")
-    public Result<Object> publicationStaticByYear(@RequestParam(value = "type") @NotNull String type,
+    public Result<ArrayList<DataPerYear>> publicationStaticByYear(@RequestParam(value = "type") @NotNull String type,
                                                   @RequestParam(value = "id") @NotNull String id) {
+        Result<ArrayList<DataPerYear>> result = new Result<>();
         Boolean checkRes = analysisShowService.checkTargetExist(type, id);
         if (checkRes == null)
-            return new Result<>().withFailure("该类型不支持发文统计");
+            return result.withFailure("该类型不支持发文统计");
         if (!checkRes)
-            return new Result<>().withFailure("id不存在");
+            return result.withFailure(ExceptionType.NOT_FOUND);
         ArrayList<DataPerYear> data = analysisShowService.getPublicationStatic(type, id);
-        return new Result<>().withData(data);
+        return result.withData(data);
     }
 
     @ApiOperation(value = "话题统计", notes = "学者、机构、期刊发文参与的话题统计")
     @GetMapping("/topicStatic")
-    public Result<Object> topicStatic(@RequestParam(value = "type") @NotNull String type,
+    public Result<ArrayList<WordFrequency>> topicStatic(@RequestParam(value = "type") @NotNull String type,
                                       @RequestParam(value = "id") @NotNull String id) {
+        Result<ArrayList<WordFrequency>> result = new Result<>();
         Boolean checkRes = analysisShowService.checkTargetExist(type, id);
         if (checkRes == null)
-            return new Result<>().withFailure("该类型不支持话题统计");
+            return result.withFailure("该类型不支持话题统计");
         if (!checkRes)
-            return new Result<>().withFailure("id不存在");
+            return result.withFailure(ExceptionType.NOT_FOUND);
         ArrayList<WordFrequency> wordFrequencies = analysisShowService.topicFrequencyStatic(type, id);
-        return new Result<>().withData(wordFrequencies);
+        return result.withData(wordFrequencies);
     }
 
     @GetMapping("/cooperation")
-    public Result<Object> getCooperation(@RequestParam(value = "type") @NotNull String type,
+    public Result<ArrayList<Cooperation>> getCooperation(@RequestParam(value = "type") @NotNull String type,
                                          @RequestParam(value = "id") @NotNull String id) {
+        Result<ArrayList<Cooperation>> result = new Result<>();
         if (type.equals("journal"))
-            return new Result<>().withFailure("该类型不支合作关系统计");
+            return result.withFailure("该类型不支合作关系统计");
         Boolean checkRes = analysisShowService.checkTargetExist(type, id);
         if (checkRes == null)
-            return new Result<>().withFailure("该类型不支合作关系统计");
+            return result.withFailure("该类型不支合作关系统计");
         if (!checkRes)
-            return new Result<>().withFailure("id不存在");
+            return result.withFailure(ExceptionType.NOT_FOUND);
         ArrayList<Cooperation> cooperation = analysisShowService.getCooperativeRelations(type, id);
-        return new Result<>().withData(cooperation);
+        return result.withData(cooperation);
     }
 
 }
