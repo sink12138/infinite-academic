85583faa82605168d30adcc9fd317d794e3e1bd7
YQ-zhishen <yangqian_1015@icloud.com>
2021-12-03 12:36:29 +0800

    [create] new module 'scholar'

---
+++ backend/academic/modules/scholar/src/main/java/com/buaa/academic/scholar/ScholarApplication.java
@@ -0,0 +1,13 @@
+package com.buaa.academic.scholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScholarApplication.class, args);
+    }
+
+}
---
+++ backend/academic/modules/scholar/src/main/java/com/buaa/academic/scholar/handler/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.buaa.academic.scholar.handler;
+
+import com.buaa.academic.model.exception.AcademicException;
+import com.buaa.academic.model.exception.ExceptionType;
+import com.buaa.academic.model.web.Result;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletException;
+import javax.validation.ValidationException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AcademicException.class)
+    @ResponseBody
+    public Result<Void> handleCustomException(AcademicException exception) {
+        return new Result<Void>().withFailure(exception.getMessage());
+    }
+
+    @ExceptionHandler({
+            HttpMessageConversionException.class,
+            MethodArgumentNotValidException.class,
+            ValidationException.class,
+            TypeMismatchException.class,
+            ServletException.class })
+    @ResponseBody
+    public Result<Void> handleParamException() {
+        return new Result<Void>().withFailure(ExceptionType.INVALID_PARAM);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result<Void> handleOtherException(Exception exception) {
+        exception.printStackTrace();
+        return new Result<Void>().withFailure(ExceptionType.INTERNAL_ERROR);
+    }
+
+}
