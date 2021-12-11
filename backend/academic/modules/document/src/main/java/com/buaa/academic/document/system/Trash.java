c5ee6b44e1f7d105dce87ee07ff9f39736d9e0fc
Henry <52558285+MiserableFaithAI@users.noreply.github.com>
2021-12-11 23:34:28 +0800

    [fix] missing 'id' property

--- backend/academic/modules/document/src/main/java/com/buaa/academic/document/system/Trash.java
+++ backend/academic/modules/document/src/main/java/com/buaa/academic/document/system/Trash.java
@@ -3,6 +3,7 @@ package com.buaa.academic.document.system;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -13,9 +14,13 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(indexName = "message")
+@Document(indexName = "trash")
 @Setting
 public class Trash {
+
+    @Id
+    @Field(type = FieldType.Keyword)
+    private String id;
 
     @Field(type = FieldType.Keyword)
     private String title;
