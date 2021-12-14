b28516a6d5042b570c2b2c17addee5729e85cf1e
Henry <52558285+MiserableFaithAI@users.noreply.github.com>
2021-12-14 21:02:33 +0800

    处理英文机构名称

--- backend/academic/modules/spider/src/main/java/com/buaa/academic/spider/util/StringUtil.java
+++ backend/academic/modules/spider/src/main/java/com/buaa/academic/spider/util/StringUtil.java
@@ -3,12 +3,13 @@ package com.buaa.academic.spider.util;
 import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.seg.Segment;
 import com.hankcs.hanlp.seg.common.Term;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class StringUtil {
     public static String rmPlaceNameAndCode(String text) {
+        if (text.matches("^[a-zA-Z].*"))
+            return text;
         Segment segment = HanLP.newSegment();
         text = text.replace(",", " ");
         text = text.replace("，", " ");
