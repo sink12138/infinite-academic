0467ddc0ad4a68a04c3a3756696046397b5a0fc7
Qian <244901960@qq.com>
2021-12-16 21:56:34 +0800

    Apply

---
+++ frontend/src/components/MessageDialog.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    width="500"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
+      >
+        详细信息
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2">
+        Privacy Policy
+      </v-card-title>
+
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          text
+          @click="dialog = false"
+        >
+          I accept
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+  props: {
+    message: {
+      type: Object,
+      default:() => {}
+    }
+  }
+}
+</script>
--- frontend/src/views/user/Apply.vue
+++ frontend/src/views/user/Apply.vue
@@ -1,97 +1,202 @@
 <template>
-  <v-card>
-    <v-list class="text-left">
-      <v-list-group
-        v-for="item in types"
-        :key="item.type"
-        v-model="item.active"
-        v-show="item.items.length != 0"
-        no-action
-      >
-        <template v-slot:activator>
-          <v-icon>{{item.icon}}</v-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.type"></v-list-item-title>
-          </v-list-item-content>
-        </template>
+  <v-card height="100%">
+    <v-tabs
+      v-model="window"
+      dark
+      grow
+      background-color="indigo darken-1"
+    >
+      <v-tab>
+        <v-icon>mdi-timer-sand-full</v-icon>
+        审核中申请
+      </v-tab>
+      <v-tab>
+        <v-icon>mdi-cloud-check</v-icon>
+        已通过申请
+      </v-tab>
+      <v-tab>
+        <v-icon>mdi-cloud-alert</v-icon>
+        未通过申请
+      </v-tab>
+      <v-tab>
+        <v-icon>mdi-menu</v-icon>
+        所有申请
+      </v-tab>
+    </v-tabs>
 
-        <v-list-item
-          v-for="child in item.items"
-          :key="child.title"
-        >
-          <v-list-item-content>
-            <v-list-item-title v-text="child.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-group>
-    </v-list>
+    <v-window
+      v-model="window"
+    >
+
+      <!-- 审核中 -->
+      <v-window-item>
+        
+      </v-window-item>
+      
+      <!-- 已通过申请 -->
+      <v-window-item>
+        
+      </v-window-item>
+      
+      <!-- 未通过申请 -->
+      <v-window-item>
+        
+      </v-window-item>
+      
+      <!-- 所有申请 -->
+      <v-window-item>
+        <v-card>
+          <v-list
+            class="text-left"
+          >
+            <v-list-group
+              v-for="item in types"
+              :key="item.type"
+              v-show="item.items.length != 0"
+              prepend-icon="mdi-arrow-down-drop-circle"
+              sub-group
+            >
+              <template v-slot:activator>
+                <v-icon>{{item.icon}}</v-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.type"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item
+                v-for="child in item.items"
+                :key="child.title"
+                class="pl-8 item-color"
+              >
+              
+                <v-chip
+                  v-if="child.status == '审核通过'"
+                  dark
+                >
+                  <v-icon color="green lighten-3">
+                    mdi-checkbox-marked
+                  </v-icon>
+                  <span>已通过</span>
+                </v-chip>
+                <v-chip
+                  v-if="child.status == '审核不通过'"
+                  dark
+                >
+                  <v-icon color="red lighten-3">
+                    mdi-close-box
+                  </v-icon>
+                  <span>未通过</span>
+                </v-chip>
+                <v-chip
+                  v-if="child.status == '审核中'"
+                  dark
+                >
+                  <v-icon color="blue lighten-4">
+                    mdi-timer-sand-full
+                  </v-icon>
+                  <span>审核中</span>
+                </v-chip>
+
+                <v-list-item-content class="ml-2">
+                  {{child.type}} : {{child.title}}
+                </v-list-item-content>
+
+                <v-spacer></v-spacer>
+
+                <v-icon>
+                  mdi-clock
+                </v-icon>
+                <v-list-item-content>
+                  {{child.time}}
+                </v-list-item-content>
+                
+                <v-spacer></v-spacer>
+
+                <message-dialog :message="child"></message-dialog>
+                
+              </v-list-item>
+              <v-divider></v-divider>
+            </v-list-group>
+          </v-list>
+        </v-card>
+      </v-window-item>
+    </v-window>
+
   </v-card>
 </template>
 
 <script>
+import MessageDialog from "../../components/MessageDialog.vue"
 export default {
+  components: {
+    MessageDialog,
+  },
   data() {
     return {
       types: [
         {
           type: "学者认证",
-          active: false,
-          icon: "mdi-account",
+          icon: "mdi-school-outline",
           items: [
-            { title: "test" },
-            { title: "test" },
-            { title: "test" },
+            { 
+              type: "修改",
+              title: "认证申请",
+              status: "审核不通过",
+              "time": "2021-08-17 19:26",
+            },
           ]
         },
         {
-          type: "门户认领",
-          active: false,
-          icon: "mdi-account",
+          type: "门户管理",
+          icon: "mdi-home-outline",
           items: [
-            { title: "test" }
+            { 
+              type: "修改",
+              title: "门户修改",
+              status: "审核中",
+              "time": "2021-08-17 19:26",
+            }
           ]
         },
         {
-          type: "修改门户",
-          active: false,
-          icon: "mdi-account",
+          type: "论文管理",
+          icon: "mdi-book-open-page-variant-outline",
           items: [
-            { title: "test" }
-          ]
-        },
-        {
-          type: "添加论文",
-          active: false,
-          icon: "mdi-account",
-          items: [
-            { title: "test" }
-          ]
-        },
-        {
-          type: "修改论文",
-          active: false,
-          icon: "mdi-account",
-          items: [
-            { title: "test" }
-          ]
-        },
-        {
-          type: "移除论文",
-          active: false,
-          icon: "mdi-account",
-          items: [
-            { title: "test" }
+            { 
+              type: "修改",
+              title: "title",
+              status: "审核不通过",
+              "time": "2021-08-17 19:26",
+             },
+            { 
+              type: "修改",
+              title: "title",
+              status: "审核通过",
+              "time": "2021-08-17 19:26",
+            },
+            { 
+              type: "修改",
+              title: "title",
+              status: "审核中",
+              "time": "2021-08-17 19:26",
+            },
           ]
         },
         {
           type: "专利转让",
-          active: false,
-          icon: "mdi-account",
+          icon: "mdi-swap-horizontal-circle-outline",
           items: [
-            
+            { 
+              type: "修改",
+              title: "转让",
+              status: "审核中",
+              "time": "2021-08-17 19:26",
+            }
           ]
         },
-      ]
+      ],
+      window: 0,
+      
     }
   },
   methods: {
@@ -113,6 +218,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.item-color {
+  background-color: #FAFAFA;
+}
 </style>
