792d6bf5629c2303be3661b675cad91bfe05dc64
Qian <244901960@qq.com>
2021-12-16 23:18:33 +0800

    combobox

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,63 +1,35 @@
 <template>
-  <div>
-    <PaperCard
-      v-for="item in items"
-      :key="item.id"
-      :item="item"
-    ></PaperCard>
-    <user-menu></user-menu>
-  </div>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-combobox
+          label="Combobox"
+          :value="input"
+          :items="result"
+          :search-input.sync="input"
+          @update:search-input="getData"
+          outlined
+        ></v-combobox>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import PaperCard from '../components/BasePaperCard.vue'
 
 export default {
-  components: {
-    PaperCard
-  },
   data:() =>  ({
-    "items": [
-      {
-        "abstract": "假装这是一大段摘要",
-        "authors": [
-          {
-            "id": "GF_4ynwBF-Mu8unTG1hc",
-            "name": "谭火彬"
-          }
-        ],
-        "citationNum": 114,
-        "date": "2021-10-15",
-        "id": "GF_4ynwBF-Mu8unTG1hc",
-        "journal": {
-          "id": "GF_4ynwBF-Mu8unTG1hc",
-          "title": "Science"
-        },
-        "keywords": [12,23,34],
-        "title": "基于机器学习的无需人工编制词典的切词系统",
-        "type": "期刊论文"
-      },
-      {
-        "abstract": "假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要",
-        "authors": [
-          {
-            "id": "GF_4ynwBF-Mu8unTG1hc",
-            "name": "谭火彬"
-          }
-        ],
-        "citationNum": 114,
-        "date": "2021-10-15",
-        "id": "GF_4ynwBF-Mu8unTG1hd",
-        "journal": {
-          "id": "GF_4ynwBF-Mu8unTG1hc",
-          "title": "Science"
-        },
-        "keywords": ["dasd","asdf","agagd","dasd"],
-        "title": "基于机器学习的无需人工编制词典的切词系统",
-        "type": "期刊论文"
-      },
-    ],
-  })
+    input: '',
+    result: ['dasd','123d'],
+    count: 0
+  }),
+  methods:{
+    getData() {
+      this.result.push(this.count)
+      this.count++
+      console.log(this.input)
+    }
+  }
 }
 
 </script>
