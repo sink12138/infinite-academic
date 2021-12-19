563ee83ce8201bac384bcc0ae996a79baf8cb7df
13321187597 <goutrui@163.com>
2021-12-19 16:21:24 +0800

    个人主页

--- frontend/src/views/user/Message.vue
+++ frontend/src/views/user/Message.vue
@@ -1,98 +1,110 @@
 <template>
-  <v-card
-    flat
-    outlined
-  >
-    <v-card-text>
-      <v-data-table
-        :headers="headers"
-        :items="messages"
-        :options.sync="options"
-        :server-items-length="totalMessages"
-        :loading="loading"
-        class="message"
-      >
-        <template v-slot:top>
-          <v-toolbar flat>
-            <v-toolbar-title>信息列表</v-toolbar-title>
-            <v-divider
-              inset
-              vertical
-            ></v-divider>
-            <v-spacer></v-spacer>
-            <v-checkbox
-              v-model="read"
-              :label="'是否仅显示为已读消息'"
-              @change="getMessages"
-            ></v-checkbox>
-            <v-checkbox
-              :label="'是否仅显示为未读消息'"
-              @change="getMessages"
-            ></v-checkbox>
-            <v-dialog
-              v-model="dialogDelete"
-              max-width="500px"
-            >
-              <v-card>
-                <v-card-title class="text-h5">真的要删除这条消息吗？</v-card-title>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="closeDelete()"
-                  >否</v-btn>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="submitDelete()"
-                  >是</v-btn>
-                  <v-spacer></v-spacer>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-toolbar>
-        </template>
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-icon
-            small
-            @click="deleteItem(item)"
-          >
-            mdi-delete
-          </v-icon>
-          <v-btn
-            color="primary"
-            @click="openDetail(item)"
-          >详情</v-btn>
-          <v-card v-if="details === true">
-            <v-card-title>
-              <span class="headline">{{detail.title}}</span>
-            </v-card-title>
-            <v-card-text>{{detail.time}}</v-card-text>
-            <v-card-text>{{detail.content}}</v-card-text>
-            <v-card-actions>
+  <div>
+    <v-card
+      flat
+      outlined
+    >
+      <v-card-text>
+        <v-data-table
+          :headers="headers"
+          :items="messages"
+          :options.sync="options"
+          :server-items-length="totalMessages"
+          :loading="loading"
+          class="message"
+        >
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>信息列表</v-toolbar-title>
+              <v-divider
+                inset
+                vertical
+              ></v-divider>
               <v-spacer></v-spacer>
-              <v-btn
-                color="green darken-1"
-                text
-                @click="returnList()"
-              >返回</v-btn>
-            </v-card-actions>
-          </v-card>
-        </template>
+              <v-checkbox
+                v-model="isChooseRead"
+                :label="'是否仅显示为已读消息'"
+                @click="reloadRead()"
+              ></v-checkbox>
+              <v-checkbox
+                v-model="isChooseNotRead"
+                :label="'是否仅显示为未读消息'"
+                @click="reloadNotRead()"
+              ></v-checkbox>
+              <v-dialog
+                v-model="dialogDelete"
+                max-width="500px"
+              >
+                <v-card>
+                  <v-card-title class="text-h5">真的要删除这条消息吗？</v-card-title>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="closeDelete()"
+                    >否</v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="submitDelete()"
+                    >是</v-btn>
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon
+              small
+              @click="deleteItem(item)"
+            >
+              mdi-delete
+            </v-icon>
+            <v-btn
+              color="primary"
+              @click="openDetail(item)"
+            >详情</v-btn>
+          </template>
 
-        <template v-slot:no-data>
-          <v-btn
-            color="primary"
-            @click="getMessages"
-          >
-            重置
-          </v-btn>
-        </template>
-      </v-data-table>
-    </v-card-text>
-  </v-card>
-
+          <template v-slot:no-data>
+            <v-btn
+              color="primary"
+              @click="getMessages"
+            >
+              重置
+            </v-btn>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
+    <v-card v-if="details === true">
+      <v-card-title
+        class="headline grey lighten-2"
+        primary-title
+      >
+        {{detail.title}}
+      </v-card-title>
+      <v-card-text>
+        {{detail.time}}
+      </v-card-text>
+      <v-card-text>
+        {{detail.content}}
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          text
+          @click="returnList()"
+        >
+          关闭
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -110,6 +122,8 @@ export default {
   },
   data() {
     return {
+      isChooseRead: false,
+      isChooseNotRead: false,
       dialog: false,
       dialogDelete: false,
       dialogValid: false,
@@ -122,15 +136,8 @@ export default {
       size: 10,
       details: false,
       headers: [
-        {
-          text: "ID",
-          align: "start",
-          value: "id",
-        },
         { text: "消息时间", value: "time", sortable: false },
         { text: "消息标题", value: "title", sortable: false },
-        { text: "消息正文", value: "content", sortable: false },
-        { text: "是否已读", value: "read", sortable: false },
         { text: "操作", value: "actions", sortable: false },
       ],
       messages: [],
@@ -174,13 +181,34 @@ export default {
     },
   },
   methods: {
+    reloadRead() {
+      if (this.isChooseNotRead === true) {
+        this.isChooseNotRead = false;
+      }
+      this.read = true;
+      this.getMessages();
+      if (this.isChooseRead === false && this.isChooseNotRead === false) {
+        this.getMessages();
+      }
+    },
+    reloadNotRead() {
+      if (this.isChooseRead === true) {
+        this.isChooseRead = false;
+      }
+      this.read = false;
+      this.getMessages();
+      if (this.isChooseRead === false && this.isChooseNotRead === false) {
+        this.getMessages();
+      }
+    },
     submitDelete() {
       this.deleteMessages.push(this.deleteMessage.id);
+      console.log(this.deleteMessages);
       this.$axios({
         method: "post",
         url: "/api/account/message/remove",
         data: {
-          deleteId: this.deleteMessages,
+          idList: this.deleteMessages,
         },
       }).then((response) => {
         console.log(response.data);
@@ -188,7 +216,7 @@ export default {
           this.$notify({
             title: "成功",
             message: "信息删除成功",
-            type: "warning",
+            type: "success",
           });
         } else {
           this.$notify({
@@ -202,33 +230,96 @@ export default {
       this.getMessages;
     },
     getMessages() {
-      this.loading = true;
-      this.page = this.options.page;
-      this.size =
-        this.options.itemsPerPage <= 30 ? this.options.itemsPerPage : 30;
-      this.$axios({
-        method: "get",
-        url: "/api/account/message/list",
-        params: {
-          page: this.page - 1,
-          size: this.size,
-          read: true,
-        },
-      }).then((response) => {
-        console.log(response.data);
-        if (response.data.success === true) {
-          this.messages = response.data.data.messages;
-          this.totalMessages = response.data.data.pageCount * this.size;
-          this.loading = false;
-        } else {
-          this.$notify({
-            title: "失败",
-            message: "信息获取失败",
-            type: "warning",
-          });
-          this.loading = false;
-        }
-      });
+      if (this.isChooseRead === false && this.isChooseNotRead === false) {
+        this.loading = true;
+        this.page = this.options.page;
+        this.size =
+          this.options.itemsPerPage <= 30 ? this.options.itemsPerPage : 30;
+        console.log(this.page);
+        console.log(this.size);
+        this.$axios({
+          method: "get",
+          url: "/api/account/message/list",
+          params: {
+            page: this.page - 1,
+            size: this.size,
+          },
+        }).then((response) => {
+          console.log(response.data);
+          if (response.data.success === true) {
+            this.messages = response.data.data.messages;
+            this.totalMessages = response.data.data.totalPages * this.size;
+            this.loading = false;
+          } else {
+            this.$notify({
+              title: "失败",
+              message: "信息获取失败",
+              type: "warning",
+            });
+            this.loading = false;
+          }
+        });
+      } else if (this.isChooseRead === true) {
+        this.loading = true;
+        this.page = this.options.page;
+        this.size =
+          this.options.itemsPerPage <= 30 ? this.options.itemsPerPage : 30;
+        console.log(this.page);
+        console.log(this.size);
+        this.$axios({
+          method: "get",
+          url: "/api/account/message/list",
+          params: {
+            page: this.page - 1,
+            size: this.size,
+            read: true,
+          },
+        }).then((response) => {
+          console.log(response.data);
+          if (response.data.success === true) {
+            this.messages = response.data.data.messages;
+            this.totalMessages = response.data.data.totalPages * this.size;
+            this.loading = false;
+          } else {
+            this.$notify({
+              title: "失败",
+              message: "信息获取失败",
+              type: "warning",
+            });
+            this.loading = false;
+          }
+        });
+      } else {
+        this.loading = true;
+        this.page = this.options.page;
+        this.size =
+          this.options.itemsPerPage <= 30 ? this.options.itemsPerPage : 30;
+        console.log(this.page);
+        console.log(this.size);
+        this.$axios({
+          method: "get",
+          url: "/api/account/message/list",
+          params: {
+            page: this.page - 1,
+            size: this.size,
+            read: false,
+          },
+        }).then((response) => {
+          console.log(response.data);
+          if (response.data.success === true) {
+            this.messages = response.data.data.messages;
+            this.totalMessages = response.data.data.totalPages * this.size;
+            this.loading = false;
+          } else {
+            this.$notify({
+              title: "失败",
+              message: "信息获取失败",
+              type: "warning",
+            });
+            this.loading = false;
+          }
+        });
+      }
     },
 
     returnList() {
@@ -245,13 +336,14 @@ export default {
       }).then((response) => {
         console.log(response.data);
         this.getMessages();
+        this.detail = this.defaultItem;
       });
     },
 
     openDetail(item) {
       this.detail = Object.assign({}, item);
       this.details = true;
-      console.log(this.details);
+      console.log(this.detail);
     },
 
     deleteItem(item) {
