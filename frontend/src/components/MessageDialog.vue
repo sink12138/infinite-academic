3c1056bb03bf5b5a1fc61844a70dd5cd00bd75fd
Qian <244901960@qq.com>
2021-12-20 12:32:16 +0800

    details

--- frontend/src/components/MessageDialog.vue
+++ frontend/src/components/MessageDialog.vue
@@ -15,14 +15,65 @@
       </v-btn>
     </template>
 
-    <v-card>
+    <v-card class="text-left">
       <v-card-title class="text-h5 grey lighten-2">
-        Privacy Policy
+        {{message.title}}
       </v-card-title>
 
-      <v-card-text>
-        {{message.title}}
+      <div v-if="this.type == 'certification'">
+        <div 
+          v-if="this.content.claim != null"
+          class="d-inline-flex"
+        >
+          <v-card-text class="font-weight-black">
+            申请认领门户ID:
+          </v-card-text>
+          <v-card-text>
+            <span
+              v-for="portal in this.content.claim.portals"
+              :key="portal"
+            >
+              {{portal}}
+            </span>
+          </v-card-text>
+        </div>
+        <div v-else>
+
+        </div>
+      </div>
+
+      <v-card-text v-if="this.type == 'claim'">
+        
       </v-card-text>
+
+      <v-card-text v-if="this.type == 'edit_paper'">
+        
+      </v-card-text>
+
+      <v-card-text v-if="this.type == 'modification'">
+        
+      </v-card-text>
+
+      <v-card-text v-if="this.type == 'new_paper'">
+        
+      </v-card-text>
+
+      <v-card-text v-if="this.type == 'remove_paper'">
+        
+      </v-card-text>
+
+      <v-card-text v-if="this.type == 'transfer'">
+        
+      </v-card-text>
+
+      <div class="d-inline-flex">
+        <v-card-text class="font-weight-black">
+          申请时间:
+        </v-card-text>
+        <v-card-text>
+          {{message.time}}
+        </v-card-text>
+      </div>
 
       <v-divider></v-divider>
 
@@ -33,7 +84,7 @@
           text
           @click="dialog = false"
         >
-          I accept
+          关闭
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -45,6 +96,28 @@ export default {
   data() {
     return {
       dialog: false,
+      type: "certification",
+      content: {
+        "claim": {
+          "portals": [12,23,34]
+        },
+        "create": {
+          "currentInst": {
+            "id": "",
+            "name": ""
+          },
+          "gIndex": 0,
+          "hIndex": 0,
+          "institutions": [
+            {
+              "id": "",
+              "name": ""
+            }
+          ],
+          "interests": [],
+          "name": ""
+        }
+      }
     }
   },
   props: {
@@ -63,7 +136,29 @@ export default {
       }).catch(error => {
         console.log(error)
       })
-    }
+    },
+    certification(content) {
+      this.type = "certification"
+      this.content = content
+    },
+    /*claim(content) {
+      this.type = "claim"
+    },
+    editPaper(content) {
+      this.type = "edit_paper"
+    },
+    modification(content) {
+      this.type = "modification"
+    },
+    newPaper(content) {
+      this.type = "new_paper"
+    },
+    removePaper(content) {
+      this.type = "remove_paper"
+    },
+    transfer(content) {
+      this.type = "transfer"
+    }*/
   }
 }
 </script>
