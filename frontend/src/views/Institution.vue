fc983c46cf952ebc609d9efc6a5c215645f06851
lessoly <wysnjjq@163.com>
2021-12-21 13:03:44 +0800

    已经将四个实体分为详细+关联，已实现跳转，下一步写关联信息的不同card

--- frontend/src/views/Institution.vue
+++ frontend/src/views/Institution.vue
@@ -14,21 +14,6 @@
               <h1>
                 {{name}}
               </h1>
-              <h2>机构学者:</h2>
-              <v-row v-for="item in authors" :key="item.id">
-                <v-col  offset-md="1"  cols="1">
-                  <p>{{item.name}}</p>
-                </v-col>
-                <v-col cols="1">
-                  <p>{{item.interests}}</p>
-                </v-col>
-                <v-col cols="1">
-                  <p>{{item.paperNum}}</p>
-                </v-col>
-                <v-col cols="1">
-                  <p>{{item.patentNum}}</p>
-                </v-col>
-              </v-row>
             </div>
           </v-col>
         </v-row>
@@ -39,126 +24,37 @@
         height="400"
         aspect-ratio="16/9"
       ></v-img>
-      <v-row
-        v-for="i in onePageNum"
-        :key="i"
-        justify="center"
-      >
-        <v-col>
-          <PaperCard :item="papers[i+(page-1)*onePageNum-1]" style="margin:0 auto;left:0;right:0;"></PaperCard>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-pagination
-            v-model="page"
-            class="my-4"
-            :length="8>pageNum?pageNum:8"
-          ></v-pagination>
-        </v-col>
-      </v-row>
+      <PaperTabs :styles="styles" :publications="publications" :scholars="scholars"></PaperTabs>
     </div>
   </div>
 </template>
 
 <script>
 import Banner from '../components/BaseBanner.vue'
-import PaperCard from '../components/card/CardPaper.vue'
 import { publishChart } from '../components/mixins/mixin'
-
+import PaperTabs from '../components/PaperTabs.vue'
 export default {
   mixins: [publishChart],
   components: {
     Banner,
-    PaperCard,
+    PaperTabs
   },
-  data() {
-    return {
-      data:[3,2,4,3,2],
-      id:"GF_4ynwBF-Mu8unTG1hc",
-      name:"机构名",
-      interests:"科研方向",
-      paperNum:0,
-      patentNum:0,
-      logoUrl:"https://ma-v3-images.azureedge.net/images/149899117.png",
-      page:1,
-      onePageNum:1,
-      pageNum:2,
-      papers: [
-        {
-          "abstract": "假装这是一大段摘要",
-          "authors": [
-            {
-              "id": "GF_4ynwBF-Mu8unTG1hc",
-              "name": "谭火彬"
-            }
-          ],
-          "citationNum": 114,
-          "date": "2021-10-15",
-          "id": "GF_4ynwBF-Mu8unTG1hc",
-          "journal": {
-            "id": "GF_4ynwBF-Mu8unTG1hc",
-            "title": "Science"
-          },
-          "keywords": [12,23,34],
-          "title": "基于机器学习的无需人工编制词典的切词系统",
-          "type": "期刊论文"
-        },
-        {
-          "abstract": "假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要假装这是一大段摘要",
-          "authors": [
-            {
-              "id": "GF_4ynwBF-Mu8unTG1hc",
-              "name": "谭火彬"
-            }
-          ],
-          "citationNum": 114,
-          "date": "2021-10-15",
-          "doi":"1234123",
-          institutions:[
-            {
-              id:"in1",
-              name:"ins1"
-            }
-          ],
-          publisher:"asda",
-          referencePapers:[
-            {
-              id:"1",
-              title:"rep1"
-            }
-          ],
-          "subjects":["ma","ca","ph"],
-          "id": "GF_4ynwBF-Mu8unTG1hd",
-          "journal": {
-            "id": "GF_4ynwBF-Mu8unTG1hc",
-            "title": "Science"
-          },
-          "keywords": ["dasd","asdf","agagd","dasd"],
-          "title": "基于机器学习的无需人工编制词典的切词系统",
-          "type": "期刊论文"
-        },
-      ],
-      authors:[
-        {
-          id:1,
-          name:"a",
-          interests:"math",
-          paperNum:100,
-          patentNum:99,
-        },
-        {
-          id:2,
-          name:"d",
-          interests:"wath",
-          paperNum:1000,
-          patentNum:992,
-        }
-      ],
+  data(){
+    return{
+      id:{},
+      styles:"institutions",
+      publications:{},
+      scholars:{},
+      logoUrl:"",
+      name:"",
     }
   },
   mounted() {
-    this.initChart();
+    this.id = this.$route.query.id
+    this.initChart()
+    this.getInfo()
+    this.getPublications()
+    this.getScholars()
   },
   methods:{
     getInfo(){
@@ -169,94 +65,58 @@ export default {
           id: this.id
         }
       }).then(response => {
-        console.log(response.data)
-        this.avatarUrl=response.data.avatarUrl
-        this.name=response.data.name
+        if(!response.data.success){
+          console.log(response.data.message)
+        }else{
+          console.log(response.data)
+          this.logoUrl=response.data.data.logoUrl
+          this.name=response.data.data.name
+        }
       }).catch(error => {
         console.log(error)
       })
     },
-    getPapers(){
-      this.papers=[]
-      let aPaper=Object
-      let hasMore=false
-      let page=0
-      this.$axios({
-        method: "get",
-        url: "/search/relation/publications/institution/"+this.id+"/"+page,
-        params: {
-          entity:"institution",
-          id: this.id,
-          page:page,
-        }
-      }).then(response => {
-        console.log(response.data)
-        aPaper=response.data.item
-        hasMore=response.data.hasMore
-        this.papers.push(aPaper)
-      }).catch(error => {
-        console.log(error)
-      })
-      while (hasMore) {
-          this.$axios({
+    getPublications(){
+        let page=0
+        this.$axios({
           method: "get",
           url: "/search/relation/publications/institution/"+this.id+"/"+page,
           params: {
-            entity:"researcher",
+            entity:"institution",
             id: this.id,
             page:page,
           }
         }).then(response => {
-          console.log(response.data)
-          aPaper=response.data.item
-          hasMore=response.data.hasMore
-          this.papers.push(aPaper)
+          console.log(response.data);
+          if(!response.data.success){
+            console.log(response.data.message);
+          }else{
+            this.publications=response.data.data
+          }
         }).catch(error => {
           console.log(error)
         })
-      }
-      this.pageNum=page+1
-    },
-    getAuthors(){
-      this.authors=[]
-      let aAuthor=Object
-      let hasMore=false
-      let page=0
-      this.$axios({
-        method: "get",
-        url: "/search/relation/scholars/"+this.id+"/"+page,
-        params: {
-          entity:"institution",
-          id: this.id,
-          page:page,
-        }
-      }).then(response => {
-        console.log(response.data)
-        aAuthor=response.data.item
-        hasMore=response.data.hasMore
-        this.papers.push(aAuthor)
-      }).catch(error => {
-        console.log(error)
-      })
-      while (hasMore) {
-          this.$axios({
+      },
+    getScholars(){
+        let page=0
+        this.$axios({
           method: "get",
-          url: "/search/relation/scholars/"+this.id+"/"+page,
+          url: "api/search/relation/scholars/"+this.id+"/"+page,
           params: {
-            entity:"researcher",
             id: this.id,
             page:page,
           }
         }).then(response => {
-          console.log(response.data)
-          aAuthor=response.data.item
-          hasMore=response.data.hasMore
-          this.papers.push(aAuthor)
+          console.log(response.data);
+          if(!response.data.success){
+            console.log(response.data.message);
+          }else{
+            this.scholars=response.data.data
+          }
         }).catch(error => {
           console.log(error)
         })
       }
-    }
   }
 }
 </script>
--- frontend/src/views/Paper.vue
+++ frontend/src/views/Paper.vue
@@ -1,62 +1,48 @@
 <template>
   <v-main>
     <Banner :title="{text: 'Paper', icon: 'mdi-book-open-blank-variant'}"></Banner>
-    <BasePaper v-bind:paperdata="this.paperdata" v-bind:references="this.references" :citations="this.citations"></BasePaper>
+    <BasePaper v-bind:paperdata="this.paperdata"></BasePaper>
+    <PaperTabs :citations="citations" :references="references" :styles="styles"></PaperTabs>
   </v-main>
 </template>
 
 <script>
 import Banner from '../components/BaseBanner.vue'
 import BasePaper from '../components/BasePaper.vue'
+import PaperTabs from '../components/PaperTabs.vue'
 export default {
   components: {
-    Banner,BasePaper
-  },
-  props:{
-    id:{}
+    Banner,BasePaper,PaperTabs
   },
   data(){
     return{
+      id:{},
       paperdata: {},
       references:{},
       citations:{},
+      styles:'paper'
     }
   },
   mounted() {
     this.id = this.$route.query.id
     console.log(this.id)
-    //本地测试
-    // this.$axios.get('../../paper.json').then(res=>{
-    //   this.paperdata=res.data.data;
-    // }),
-    //与后端对接
     this.$axios.get('api/search/info/paper/'+this.id).then(res=>{
       if(!res.data.success){
-        alert(res.data.message);
+        console.log(res.data.message);
       }else{
         this.paperdata=res.data.data;
       }
     })
-    //本地
-    // this.$axios.get('../../references.json').then(res=>{
-    //   this.references=res.data.data;
-    // }),
-    //后端对接
     this.$axios.get('/api/search/relation/references/'+this.paperdata.id+'/'+0).then(res=>{
      if(!res.data.success){
-        alert(res.data.message);
+        console.log(res.data.message);
       }else{
         this.references=res.data.data;
        }
     }),
-    //本地
-    // this.$axios.get('../../citations.json').then(res=>{
-    //   this.citations=res.data.data;
-    // })
-    //后端对接
     this.$axios.get('/api/search/relation/citations/'+this.paperdata.id+'/'+0).then(res=>{
      if(!res.data.success){
-        alert(res.data.message);
+        console.log(res.data.message);
       }else{
         this.citations=res.data.data;
        }
