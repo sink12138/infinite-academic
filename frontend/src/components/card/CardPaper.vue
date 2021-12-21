b0ed0366ed234dc8c2e0de4bc75e308f67553239
13321187597 <goutrui@163.com>
2021-12-21 21:46:17 +0800

    专利转移界面修改

--- frontend/src/components/card/CardPaper.vue
+++ frontend/src/components/card/CardPaper.vue
@@ -7,14 +7,23 @@
       <v-icon class="mx-1">
         mdi-text-box-multiple-outline
       </v-icon>
-      <a @click="href('paper', item.id)" v-html="item.title"></a>
+      <a
+        @click="href('paper', item.id)"
+        v-html="item.title"
+      ></a>
       <v-spacer></v-spacer>
-      <v-btn icon @click="addCitationItem(item)">
+      <v-btn
+        icon
+        @click="addCitationItem(item)"
+      >
         <v-icon>mdi-comma-box</v-icon>
       </v-btn>
     </v-card-title>
     <v-card-subtitle class="pb-0">
-      <span v-if="item.date" v-text="item.date.substr(0,4)"></span>&nbsp;
+      <span
+        v-if="item.date"
+        v-text="item.date.substr(0,4)"
+      ></span>&nbsp;
       <a
         v-if="item.journal"
         @click="href('journal', item.journal.id)"
@@ -60,9 +69,14 @@
       </span>
     </v-card-text>
     <v-card-text>
-      <span v-if="expand" v-html="item.abstract"></span>
-      <span v-else v-html="$options.filters.abstract(item.abstract)"></span>
-
+      <span
+        v-if="expand"
+        v-html="item.abstract"
+      ></span>
+      <span
+        v-else
+        v-html="$options.filters.abstract(item.abstract)"
+      ></span>
       <v-btn
         x-small
         outlined
@@ -71,60 +85,63 @@
       >
         <span v-if="expand">收起</span>
         <span v-else>全部</span>
-
-        <v-icon small v-if="expand">mdi-chevron-up</v-icon>
-        <v-icon small v-else>mdi-chevron-down</v-icon>
+        <v-icon
+          small
+          v-if="expand"
+        >mdi-chevron-up</v-icon>
+        <v-icon
+          small
+          v-else
+        >mdi-chevron-down</v-icon>
       </v-btn>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
-import { addCitation } from "../mixins/mixin"
+import { addCitation } from "../mixins/mixin";
 export default {
   mixins: [addCitation],
-  props:{
+  props: {
     item: {
       type: Object,
-      default:() => {}
-    }
+      default: () => {},
+    },
   },
   data() {
     return {
-      expand: false
-    }
+      expand: false,
+    };
   },
   filters: {
     abstract(text) {
       if (!text) return " ";
       if (text.length > 110) {
-        return text.slice(0,110) + "..."
+        return text.slice(0, 110) + "...";
       }
       return text;
-    }
+    },
   },
   methods: {
     href(type, id) {
-      var this_path = this.$route.path.substring(1)
-      var this_id = ""
-      if (this.$route.query.id != null)
-        this_id = this.$route.query.id
-      else if (this.$route.query.name !=null)
-        this_id = this.$route.query.name
+      var this_path = this.$route.path.substring(1);
+      var this_id = "";
+      if (this.$route.query.id != null) this_id = this.$route.query.id;
+      else if (this.$route.query.name != null) this_id = this.$route.query.name;
       if (type == this_path && id == this_id) return;
       if (type == "topic" || type == "subject")
         this.$router.push({
           path: type,
-          query: { name: id }
-        })
+          query: { name: id },
+        });
       else
         this.$router.push({
           path: type,
-          query: { id: id }
-        })
-    }
-  }
-}
+          query: { id: id },
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -140,7 +157,7 @@ a:visited {
   text-decoration: none;
 }
 a:hover {
-  color: #0D47A1;
+  color: #0d47a1;
   text-decoration: underline;
 }
 </style>
