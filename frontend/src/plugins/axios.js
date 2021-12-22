28a6ad333c423aad5dbee9500991ab088dfa7ffc
13321187597 <goutrui@163.com>
2021-12-22 13:30:45 +0800

    未读信息显示

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -2,4 +2,5 @@ import Vue from 'vue'
 import axios from 'axios'
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+axios.defaults.withCredentials = true
 Vue.prototype.$axios = axios
