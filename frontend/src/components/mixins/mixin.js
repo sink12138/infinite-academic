316718b77511342d70bb9377ca34d4c05e39c5b5
cdg <271105328@qq.com>
2021-12-22 23:43:21 +0800

    APA引用格式

--- frontend/src/components/mixins/mixin.js
+++ frontend/src/components/mixins/mixin.js
@@ -9,8 +9,12 @@ export const addCitation = {
           var citation = {
             paperId: item.id,
             MLA: {},
+            APA: {},
+            GBT: {},
           };
           citation["MLA"] = this.MLACitation(item);
+          citation["APA"] = this.APACitation(item);
+          citation["GBT"] = this.GBTCitation(item);
           citationList[item.id] = citation;
           localStorage.setItem("citations", JSON.stringify(citationList));
           this.$store.commit('incCitations');
@@ -37,6 +41,70 @@ export const addCitation = {
         else
           text += "."
       }
+      return text;
+    },
+    APACitation(item) {
+      /* 作者 */
+      var text = item.authors[0].name;
+      if (item.authors[0].length > 2) {
+        text += " et al. ";
+      }
+      else if (item.authors[0].length == 2) {
+        text += " and " + item.authors[1].name + " ";
+      }
+      else {
+        text += " ";
+      }
+
+      /* 出版年份 */
+      text += item.year + " ";
+
+      /* 文章标题 */
+      text += item.title + " ";
+
+      if (item.journal != null) {
+        /* 期刊标题(应当为斜体) */
+        text += item.journal.title + " ";
+
+        /* 卷 + 期数 + 页码*/
+        text += item.journal.volume + "(" + item.journal.issue + ")" + " n.pag ";
+      }
+      
+      /* DOI */
+      text += "doi:" + item.doi
+
+      return text;
+    },
+    GBTCitation(item) {
+      /* 作者 */
+      var text = item.authors[0].name;
+      if (item.authors[0].length > 2) {
+        text += " et al. ";
+      }
+      else if (item.authors[0].length == 2) {
+        text += " and " + item.authors[1].name + " ";
+      }
+      else {
+        text += " ";
+      }
+
+      /* 出版年份 */
+      text += item.year + " ";
+
+      /* 文章标题 */
+      text += item.title + " ";
+
+      if (item.journal != null) {
+        /* 期刊标题 */
+        text += item.journal.title + " ";
+
+        /* 卷 + 期数 + 页码*/
+        text += item.journal.volume + "(" + item.journal.issue + ")" + " n.pag ";
+      }
+      
+      /* DOI */
+      text += "doi:" + item.doi
+      
       return text;
     }
   },
